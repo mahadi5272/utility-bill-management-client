094c0ce66f4eb82b4a94a46ff397cc7770bc2e38
mahadi5272 <mahadihasan5272@gmail.com>
2025-11-10 07:12:34 +0600

    added firebase observer

--- src/routes/AuthProvider.jsx
+++ src/routes/AuthProvider.jsx
@@ -1,13 +1,14 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../Firebase.config";
+import { auth,onAuthStateChanged } from "../Firebase.config";
 import { signInWithEmailAndPassword } from "firebase/auth/cordova";
 import { GoogleAuthProvider } from "firebase/auth";
 
 export const AuthContext = createContext();
 const google = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
-  
+  const [user,setuser] = useState(null);
+  const [loading,setloading] = useState(true)
 
   // signup
   const signup = (email, password) => {
@@ -26,6 +27,14 @@ const AuthProvider = ({ children }) => {
   const signInWithGoogle =()=>{
     return signInWithGoogle(auth,google)
   }
+
+  useEffect(()=>{
+    const unsubscrib = onAuthStateChanged(auth,(currentuser)=>{
+      setuser(currentuser);
+      setloading(false)
+    })
+    return ()=>unsubscrib()
+  },[])
   const authInfo = {
     signup,
     signIn,
